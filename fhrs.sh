202e79156034239687ed1c57c73744c86dc56522
Jez Nicholson <jez.nicholson@gmail.com>
2018-05-25 13:49:34 +0100

    first cut of fhrs

--- fhrs.sh
+++ fhrs.sh
@@ -1,4 +1,28 @@
+# NB postgres demands that files are in its data dir to import them
+PGDATA=`psql -d osm-btn -Atc "SHOW data_directory;"`
 # Brighton
-curl http://ratings.food.gov.uk/OpenDataFiles/FHRS875en-GB.xml > FHRS875en-GB.xml
+if [ -e "${PGDATA}/FHRS875en-GB-$(date +'%Y-%m-%d').xml" ]
+then
+  echo "file already downloaded today"
+else
+  curl http://ratings.food.gov.uk/OpenDataFiles/FHRS875en-GB.xml > "${PGDATA}/FHRS875en-GB-$(date +'%Y-%m-%d').xml"
+fi
 # Manchester
-curl http://ratings.food.gov.uk/OpenDataFiles/FHRS415en-GB.xml > FHRS415en-GB.xml
+if [ -e "${PGDATA}/FHRS415en-GB-$(date +'%Y-%m-%d').xml" ]
+then
+  echo "file already downloaded today"
+else
+  curl http://ratings.food.gov.uk/OpenDataFiles/FHRS415en-GB.xml > "${PGDATA}/FHRS415en-GB-$(date +'%Y-%m-%d').xml"
+fi
+
+psql -d osm-btn -c "SELECT \
+     (xpath('//FHRSID/text()', myTempTable.myXmlColumn))[1]::text AS id \
+    ,(xpath('//LocalAuthorityBusinessID/text()', myTempTable.myXmlColumn))[1]::text AS LocalAuthorityBusinessID \
+    ,myTempTable.myXmlColumn as myXmlElement \
+FROM unnest( \
+    xpath \
+    (    '//EstablishmentDetail' \
+        ,XMLPARSE(DOCUMENT convert_from(pg_read_binary_file('FHRS875en-GB-$(date +'%Y-%m-%d').xml'), 'UTF8')) \
+    ) \
+) AS myTempTable(myXmlColumn) \
+;"
